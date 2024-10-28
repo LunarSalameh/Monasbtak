216e5d18859e724a545ced13db9c821201b9a1dd
LunarSalameh <lun20190983@std.psut.edu.jo>
2024-10-28 16:07:51 +0300

    1st Update for Files Structure

---
+++ client/src/app/page.js
@@ -0,0 +1,10 @@
+
+import LandingPage from "./pages/landingPage/page";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen p-8 pb-20 ">
+      <LandingPage/>
+    </div>
+  );
+}
