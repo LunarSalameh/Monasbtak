2d37639b4ba282b02c709cd28f999ca6cfccc07d
LunarSalameh <lun20190983@std.psut.edu.jo>
2024-10-29 09:58:19 +0300

    added planner, admin and customer structure

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,10 +1,8 @@
 
-import LandingPage from "./pages/landingPage/page";
 
 export default function Home() {
   return (
     <div className="min-h-screen p-8 pb-20 ">
-      <LandingPage/>
     </div>
   );
 }
